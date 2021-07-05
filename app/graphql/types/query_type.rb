30a279a45d43f6a65f1b1cd35c6b29ac7d9aa1aa
Paul John Butad <butadpj@gmail.com>
2021-07-05 19:32:09 +0800

    Add User Authentication feature

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,17 +1,17 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add `node(id: ID!) and `nodes(ids: [ID!]!)`
-    include GraphQL::Types::Relay::HasNodeField
-    include GraphQL::Types::Relay::HasNodesField
+    field :users, Types::UserType, null: true do
+      description 'Query all users'
+    end
+    def users
+      User.all
+    end
 
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
-
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :current_user, Types::UserType, null: true do
+      description 'Query the current logged in user'
+    end
+    def current_user
+      context[:current_user]
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-
   devise_for :users
   if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+    authenticate :user do
+      mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
+    end
   end
-  
-  post "/graphql", to: "graphql#execute"
+
+  post '/graphql', to: 'graphql#execute'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
