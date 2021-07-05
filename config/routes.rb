2ed897d0295d0cd024b34b47b3f2af2ad6919307
Paul John Butad <butadpj@gmail.com>
2021-07-06 03:58:43 +0800

    Remove devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
-  if Rails.env.development?
-    authenticate :user do
-      mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
-    end
-  end
+  mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql' if Rails.env.development?
 
   post '/graphql', to: 'graphql#execute'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
