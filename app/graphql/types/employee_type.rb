e3bfd5d1bfc4212533dd7607208c8339ebfd99e4
Paul John Butad <butadpj@gmail.com>
2021-07-05 20:37:04 +0800

    dAd GraphQL Types for Employee, EmployeeType, & Type

--- app/graphql/types/employee_type.rb
+++ app/graphql/types/employee_type.rb
@@ -1,7 +1,7 @@
 module Types
   class EmployeeType < Types::BaseObject
     field :id, ID, null: false
-    field :user_id, Integer, null: false
+    field :user, Types::UserType, null: false, method: :user
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
 
