0052b6058d809c4596a9f16eb34f0a5b21b5c01e
Paul John Butad <butadpj@gmail.com>
2021-07-06 14:58:44 +0800

    Revert back to old crypt method

--- app/graphql/mutations/login_user.rb
+++ app/graphql/mutations/login_user.rb
@@ -19,7 +19,7 @@ module Mutations
       return unless @user.authenticate(credentials[:password])
 
       # use Ruby on Rails - ActiveSupport::MessageEncryptor, to build a token
-      crypt = ActiveSupport::MessageEncryptor.new(Rails.application.credentials.secret_key_base.byteslice(0..31))
+      crypt = ActiveSupport::MessageEncryptor.new(Rails.application.secrets.secret_key_base.byteslice(0..31))
       @token = crypt.encrypt_and_sign("user-id:#{@user.id}")
 
       context[:session][:token] = @token
